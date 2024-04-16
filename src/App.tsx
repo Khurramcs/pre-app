16e84f67e19f4afef1ffcfa900042fce428ce0eb
Muhammad Khurram <iamkhurramcs@gmail.com>
2024-04-16 18:43:22 +0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <h1 className='mb-4'>
+        Hello World
+      </h1>
+      <p>Muhammad Khurram</p>
+    </>
+  )
+}
+
+export default App
